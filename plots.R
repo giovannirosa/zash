950a20e03f3ccdd147d2e621c0dfb5afc2b58826
Giovanni <giovanni_rosa4@hotmail.com>
2021-06-21 01:20:32 -0300

    plots and pr

--- plots.R
+++ plots.R
@@ -17,11 +17,6 @@ dc <- c()
 ul <- c()
 ri <- c()
 
-df = expand.grid(a = dc, b = ul)
-df[order(df$a), ]
-
-limit <- 100
-
 for (i in 1:10) {
   for (j in 1:10) {
     dc <- c(dc, i)
@@ -30,28 +25,60 @@ for (i in 1:10) {
   }
 }
 
-print(dc)
-
 df <- data.frame(dc,ul,ri)
 
 ggplot(df, aes(x=dc, y=ul, size = ri)) +
-  geom_point(alpha=0.5, color="darkgreen") + 
+  geom_point(alpha=0.5, color="darkblue") + 
   theme(
-    axis.line = element_line(colour = "black", size = .5, linetype = "solid"),
+    axis.line = element_line(colour = "black", size = .5, linetype = "solid", arrow = grid::arrow(length = unit(0.3, "cm"))),
     axis.ticks.length = unit(.2, "cm"),
     panel.background = element_rect(fill = "white", colour="white"),
     panel.grid.major = element_line(colour = "gray", size = .1)
   ) + 
   scale_y_discrete(limits = 1:10) +
   scale_x_discrete(limits = 1:10) +
-  labs(x="Number of Device Class", y="Number of Action", size="SD") +
-  scale_color_manual(values = c("8" = "red")) + 
-  scale_size_continuous(limits = c(1,100))
+  labs(x="Number of Device Class", y="Number of User Level", size="RI")
+
+ggplot(df, aes(x=dc, y=ul, size = ri)) +
+  geom_point(alpha=0.5, color="darkgreen") + 
+  theme(
+    axis.line = element_line(colour = "black", size = .5, linetype = "solid", arrow = grid::arrow(length = unit(0.3, "cm"))),
+    axis.ticks.length = unit(.2, "cm"),
+    panel.background = element_rect(fill = "white", colour="white"),
+    panel.grid.major = element_line(colour = "gray", size = .1)
+  ) + 
+  scale_y_discrete(limits = 1:10) +
+  scale_x_discrete(limits = 1:10) +
+  labs(x="Number of Device Class", y="Number of Action", size="SD")
+
+
+
+
   
 
-data %>%
-  arrange(desc(pop)) %>%
-  mutate(country = factor(country, country)) %>%
-  ggplot(aes(x=gdpPercap, y=lifeExp, size=pop, color=continent)) +
-  geom_point(alpha=0.5) +
-  scale_size(range = c(.1, 24), name="Population (M)")
+au <- c()
+cd <- c()
+pp <- c()
+
+for (i in 1:10) {
+  for (j in 1:10) {
+    au <- c(au, i)
+    cd <- c(cd, j)
+    pp <- c(pp, i*j)
+  }
+}
+
+df <- data.frame(au,cd,pp)
+
+
+ggplot(df, aes(x=cd, y=au, size = pp)) +
+  geom_point(alpha=0.5, color="darkred") + 
+  theme(
+    axis.line = element_line(colour = "black", size = .5, linetype = "solid", arrow = grid::arrow(length = unit(0.3, "cm"))),
+    axis.ticks.length = unit(.2, "cm"),
+    panel.background = element_rect(fill = "white", colour="white"),
+    panel.grid.major = element_line(colour = "gray", size = .1)
+  ) + 
+  scale_y_discrete(limits = 1:10) +
+  scale_x_discrete(limits = 1:10) +
+  labs(x="Number of Critical Devices", y="Number Admin Users", size="PR")
